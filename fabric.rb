523ab69ffd23d56c3819866bd94d97bbd2fc9300
Ryan Senkbeil <ryan.senkbeil@gsdesign.com>
2013-09-19 08:51:09 -0500

    Fix fabric deps issues.

--- fabric.rb
+++ fabric.rb
@@ -31,13 +31,20 @@ class Fabric < Formula
     python do
       Paramiko.new.brew { system python, *install_args }
 
+      # The "main" fabric module is installed in the default location and
+      # in order for it to be usable, we add the private_site_packages
+      # to the __init__.py of fabric so the deps (Paramiko, etc) are found.
+      inreplace 'fabric/__init__.py' do |s|
+        s.gsub! /\.\n(""")/, "\"\"\"\nimport site; site.addsitedir('#{python.private_site_packages}')"
+      end
+
       system python, "setup.py", "install", "--prefix=#{prefix}",
                                             "--single-version-externally-managed",
                                             "--record=installed.txt"
     end
 
     Dir["#{bin}/*"].each do |bin_file|
-      wrap bin_file, python.site_packages
+      wrap bin_file, python.private_site_packages
     end
   end
 end
